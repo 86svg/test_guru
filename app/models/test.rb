851ce00be4e3053b23964067a38917d7146e92a5
86svg <86svg@mail.ru>
2024-06-16 20:22:06 +0300

    add less_4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,15 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+
+  has_many :questions, dependent: :destroy
+  has_many :test_passings, dependent: :destroy
+  has_many :users, through: :test_passings
+
+  def self.sorted_tests_by_category(category_name)
+    joins(:category)
+      .where(category: { title: category_name })
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,97 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# frozen_string_literal: true
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# Пользователи
+max, kate, viky =
+  User.create!([
+                 { name: 'Max', email: 'max@mail.ru' },
+                 { name: 'Kate', email: 'kate@mail.ru' },
+                 { name: 'Viky', email: 'viky@mail.ru' }
+               ])
+
+# Категории
+history, geography =
+  Category.create!([
+                     { title: 'history' },
+                     { title: 'geography' }
+                   ])
+
+# Тесты
+test_history_level1, test_history_level3, test_geography_level2, test_geography_level1 =
+  Test.create!([
+                 { title: 'history', level: 1, category: history, author: max },
+                 { title: 'history', level: 3, category: history, author: max },
+                 { title: 'geography', level: 2, category: geography, author: kate },
+                 { title: 'geography', level: 1, category: geography, author: viky }
+               ])
+
+# Вопросы и ответы можно добавить аналогично, если они необходимы
+# Пример добавления вопросов:
+history_question1, history_question2 =
+  test_history_level1.questions.create!([
+                                          { body: 'Когда началась Вторая мировая война?' },
+                                          { body: 'Кто был первым президентом США?' }
+                                        ])
+
+geography_question1, geography_question2 =
+  test_geography_level2.questions.create!([
+                                            { body: 'Какая самая длинная река в мире?' },
+                                            { body: 'В какой стране находится гора Эверест?' }
+                                          ])
+
+# Пример добавления ответов:
+history_question1.answers.create!([
+                                    { body: '1939', correct: true },
+                                    { body: '1941', correct: false },
+                                    { body: '1914', correct: false },
+                                    { body: '1936', correct: false }
+                                  ])
+
+history_question2.answers.create!([
+                                    { body: 'Джордж Вашингтон', correct: true },
+                                    { body: 'Авраам Линкольн', correct: false },
+                                    { body: 'Томас Джефферсон', correct: false },
+                                    { body: 'Джон Адамс', correct: false }
+                                  ])
+
+geography_question1.answers.create!([
+                                      { body: 'Амазонка', correct: true },
+                                      { body: 'Нил', correct: false },
+                                      { body: 'Янцзы', correct: false },
+                                      { body: 'Миссисипи', correct: false }
+                                    ])
+
+geography_question2.answers.create!([
+                                      { body: 'Непал', correct: true },
+                                      { body: 'Индия', correct: false },
+                                      { body: 'Китай', correct: false },
+                                      { body: 'Пакистан', correct: false }
+                                    ])
+
+# Добавляем прохождения тестов пользователями
+max.test_passings.create!([
+                            { status: 'пройден', test: test_history_level1 },
+                            { status: 'в процессе', test: test_geography_level2 }
+                          ])
+
+kate.test_passings.create!([
+                             { status: 'в процессе', test: test_geography_level2 },
+                             { status: 'в процессе', test: test_history_level3 }
+                           ])
+
+viky.test_passings.create!([
+                             { status: 'в процессе', test: test_history_level1 },
+                             { status: 'пройден', test: test_geography_level1 }
+                           ])
