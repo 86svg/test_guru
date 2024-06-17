16efbbccaba691e43905ac8947cd01780bef6c67
86svg <86svg@mail.ru>
2024-06-17 09:30:41 +0300

    fixing poblem

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,4 @@
 class Test < ApplicationRecord
-  belongs_to :category
-  belongs_to :author, class_name: 'User'
-
-  has_many :questions, dependent: :destroy
-  has_many :test_passings, dependent: :destroy
-  has_many :users, through: :test_passings
 
   def self.sorted_tests_by_category(category_name)
     joins(:category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ApplicationRecord
-  has_many :test_passings, dependent: :destroy
-  has_many :tests, through: :test_passings
-  has_many :created_tests,
-           class_name: 'Test',
-           foreign_key: 'author_id',
-           dependent: :destroy,
-           inverse_of: :author
 
   def tests_by_level(level)
     tests.where(level: level)
