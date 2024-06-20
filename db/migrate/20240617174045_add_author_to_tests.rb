ec105868ef7ccd636ae7262ade98fea867ca67bb
86svg <86svg@mail.ru>
2024-06-20 10:54:55 +0300

    fix#4

--- db/migrate/20240617174045_add_author_to_tests.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToTests < ActiveRecord::Migration[6.1]
-  def change
-    add_column :tests, :author, :string, null: false
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -35,51 +35,52 @@ history, geography =
 # Тесты
 test_history_level1, test_history_level3, test_geography_level2, test_geography_level1 =
   Test.create!([
-                 { title: 'history', level: 1, category_id: history.id, author_id: max.id },
-                 { title: 'history', level: 3, category_id: history.id, author_id: max.id },
-                 { title: 'geography', level: 2, category_id: geography.id, author_id: kate.id },
-                 { title: 'geography', level: 1, category_id: geography.id, author_id: viky.id }
+                 { title: 'история', level: 1, category_id: history.id, author_id: max.id },
+                 { title: 'история', level: 3, category_id: history.id, author_id: max.id },
+                 { title: 'география', level: 2, category_id: geography.id, author_id: kate.id },
+                 { title: 'география', level: 1, category_id: geography.id, author_id: viky.id }
                ])
 
 # Вопросы и ответы можно добавить аналогично, если они необходимы
 # Пример добавления вопросов:
+history_question1, history_question2 =
                       Question.create!([
-                                          { body: 'Когда началась Вторая мировая война?', test_id: history.id },
-                                          { body: 'Кто был первым президентом США?', test_id: history.id }
+                                          { body: 'Когда началась Вторая мировая война?', test_id: test_history_level1.id },
+                                          { body: 'Кто был первым президентом США?', test_id: test_history_level3.id }
                                         ])
-
+geography_question1, geography_question2 =
                         Question.create!([
-                                            { body: 'Какая самая длинная река в мире?', test_id: geography.id },
-                                            { body: 'В какой стране находится гора Эверест?', test_id: geography.id }
+                                            { body: 'Какая самая длинная река в мире?', test_id: test_geography_level2.id },
+                                            { body: 'В какой стране находится гора Эверест?', test_id: test_geography_level1.id }
                                           ])
 
 # Пример добавления ответов:
                   Answer.create!([
-                                    { body: '1939', correct: true, question_id: history.id },
-                                    { body: '1941', correct: false, question_id: history.id },
-                                    { body: '1914', correct: false, question_id: history.id },
-                                    { body: '1936', correct: false, question_id: history.id }
+                                    { body: '1939', correct: true, question_id: history_question1.id },
+                                    { body: '1941', correct: false, question_id: history_question1.id },
+                                    { body: '1914', correct: false, question_id: history_question1.id },
+                                    { body: '1936', correct: false, question_id: history_question1.id }
                                   ])
 
                   Answer.create!([
-                                    { body: 'Джордж Вашингтон', correct: true, question_id: history.id },
-                                    { body: 'Авраам Линкольн', correct: false, question_id: history.id },
-                                    { body: 'Томас Джефферсон', correct: false, question_id: history.id },
-                                    { body: 'Джон Адамс', correct: false, question_id: history.id }
+                                    { body: 'Джордж Вашингтон', correct: true, question_id: history_question2.id },
+                                    { body: 'Авраам Линкольн', correct: false, question_id: history_question2.id },
+                                    { body: 'Томас Джефферсон', correct: false, question_id: history_question2.id },
+                                    { body: 'Джон Адамс', correct: false, question_id: history_question2.id }
                                   ])
 
                     Answer.create!([
-                                      { body: 'Амазонка', correct: true, question_id: geography.id },
-                                      { body: 'Нил', correct: false, question_id: geography.id },
-                                      { body: 'Янцзы', correct: false, question_id: geography.id },
-                                      { body: 'Миссисипи', correct: false, question_id: geography.id }
+                                      { body: 'Амазонка', correct: true, question_id: geography_question1.id },
+                                      { body: 'Нил', correct: false, question_id: geography_question1.id },
+                                      { body: 'Янцзы', correct: false, question_id: geography_question1.id },
+                                      { body: 'Миссисипи', correct: false, question_id: geography_question1.id }
                                     ])
 
-                    Answer.create!([
-                                      { body: 'Непал', correct: true, question_id: geography.id },
-                                      { body: 'Индия', correct: false, question_id: geography.id },
-                                      { body: 'Китай', correct: false, question_id: geography.id },
-                                      { body: 'Пакистан', correct: false, question_id: geography.id }
+                                    Answer.create!([
+                                      { body: 'Непал', correct: true, question_id: geography_question2.id },
+                                      { body: 'Индия', correct: false, question_id: geography_question2.id },
+                                      { body: 'Китай', correct: false, question_id: geography_question2.id },
+                                      { body: 'Пакистан', correct: false, question_id: geography_question2.id }
                                     ])
 
 # Добавляем прохождения тестов пользователями
