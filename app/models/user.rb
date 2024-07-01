86b8864744574a907ec0066e2695a1928775ee8b
86svg <86svg@mail.ru>
2024-07-01 10:16:06 +0300

    fix#3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passings
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
-  def self.tests_by_level(level)
-    Test.joins(:test_passings)
-    .where(level: level, test_passings: { user_id: ids } )
+  def tests_by_level(level)
+    tests.where(level: level)
   end
 end
