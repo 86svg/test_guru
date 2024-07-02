8e89dca24c1906da763ddcbb1fb7f8bd88f9fd8a
86svg <86svg@mail.ru>
2024-07-03 00:57:38 +0300

    add less_6

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passings
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
+  validates :email, presence: true#, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, on: :create
+
   def tests_by_level(level)
     tests.where(level: level)
   end
