a3b9ccec66a2e779b3c4c042ff42023bc61c112d
86svg <86svg@mail.ru>
2024-07-03 20:10:19 +0300

    add AddUniqueIndexToTestsTitleLevel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_14_182527) do
+ActiveRecord::Schema.define(version: 2024_07_03_170622) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
