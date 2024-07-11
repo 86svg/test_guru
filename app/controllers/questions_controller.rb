67b2757e5e196043d16c06aaa1fecbef426b7947
86svg <86svg@mail.ru>
2024-07-11 10:13:37 +0300

    fix#6

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,48 +1,50 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :create, :new]
-	before_action :find_question, only: [:show, :destroy]
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :question_not_found
 
-	def index
-		@questions = @test.questions
-	end
+  def index
+    @questions = @test.questions
+  end
 
-	def show
+  def show
     render inline: '<h1> <%= @question.body %> </h1>'
-	end
+  end
 
   def new; end
 
-	def create
-		@question = @test.questions.build(question_params)
-		if @question.save
-			redirect_to @question
-		else
-		  render :new
-		end
-	end
+  def create
+    @question = @test.questions.build(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
 
-	def destroy
-		@question.destroy
-		redirect_to test_questions_path
-	end
+  def destroy
+    @question.destroy
+    redirect_to test_questions_path
+  end
 
-	private
+  private
 
-	def question_params
-		params.require(:question).permit(:body)
-	end
+  def question_params
+    params.require(:question).permit(:body)
+  end
 
-	def find_test
-		@test = Test.find(params[:test_id])
-	end
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 
-	def find_question
-		@question = Question.find(params[:id])
-	end
+  def find_question
+    @question = Question.find(params[:id])
+  end
 
-	def question_not_found
-		render plain: 'Вопрос не найден'
-	end
+  def question_not_found
+    render plain: 'Вопрос не найден'
+  end
 end
