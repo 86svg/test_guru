27500efec696cf5d96f2ac38456366ceb7fed254
86svg <86svg@mail.ru>
2024-08-19 00:50:24 +0300

    add less_12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
   def after_sign_in_path_for(resource)
     if current_user.admin?
       admin_tests_path
     else
       super(resource)
     end
+  end
+
+  def default_url_options
+    if I18n.locale == :en
+      { lang: I18n.locale }
+    else
+      { lang: nil }
+    end
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
