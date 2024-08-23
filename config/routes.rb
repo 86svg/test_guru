b87d316760d45a5df0cf43f08aacf8e68d1e8402
86svg <86svg@mail.ru>
2024-08-24 00:30:36 +0300

    fix#4

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'tests#index'
+  scope '(:lang)', lang: /#{I18n.available_locales.join("|")}/ do
+    root 'tests#index'
 
-  devise_for :users, path: :gurus,
-                     path_names: { sign_in: :login, sign_out: :logout },
-                     controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+    devise_for :users, path: :gurus,
+                      path_names: { sign_in: :login, sign_out: :logout },
+                      controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 
-  resources :tests, only: :index do
-    member do
-      post :start
+    resources :tests, only: :index do
+      member do
+        post :start
+      end
     end
-  end
 
-  resources :test_passings, only: %i[show update] do
-    member do
-      get :result
+    resources :test_passings, only: %i[show update] do
+      member do
+        get :result
+      end
     end
-  end
 
-  namespace :admin do
-    resources :tests do
-      resources :questions, except: :index, shallow: true do
-        resources :answers, except: :index, shallow: true
+    namespace :admin do
+      resources :tests do
+        resources :questions, except: :index, shallow: true do
+          resources :answers, except: :index, shallow: true
+        end
       end
     end
   end
