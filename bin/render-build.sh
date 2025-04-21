09eeec5ff24253fe1decb3809956ba94e1f25db3
86svg <86svg@mail.ru>
2025-04-21 12:45:10 +0300

    добавил скирп для сботрки приложения

---
+++ bin/render-build.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+O
+& SHANNA&D EDGE NESO∞NOU
+# AcTaHOBKa He06X0 MMbIX marMHoB Babel
+yarn install --frozen-lockfile
+# Ouncika CTaDbIX accetob
+bundle exec rails assets: clobber
+# YcTaHOBKa Ruby 3aBMChMOCTen
+bundle install
+# KOMOunAuMA ACCeTOB Webpacker
+bundle exec rails webpacker: compile
+# KoMnunAuMa acceTOB
+bundle exec rake assets:precompile
+# O4MCTKa crapbiX acceToB
+bundle exec rake assets: clean
+# AlpuMeHeHVe Murpaumü 6a3bl naHHbiX
+bundle exec rails db: migrate
+# 04MCTKa Kawa Yarn
+yarn cache clean
