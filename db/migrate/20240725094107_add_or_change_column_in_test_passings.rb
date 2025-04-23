8bdbde39d8ee2fabb84c149123b15d864c695b36
86svg <86svg@mail.ru>
2025-04-23 18:21:44 +0300

    поправил миграции

--- db/migrate/20240725094107_add_or_change_column_in_test_passings.rb
+++ db/migrate/20240725094107_add_or_change_column_in_test_passings.rb
@@ -3,6 +3,9 @@
 class AddOrChangeColumnInTestPassings < ActiveRecord::Migration[6.1]
   def change
     add_column :test_passings, :correct_questions_counter, :integer, default: 0, null: false
-    add_foreign_key :test_passings, :questions, column: :current_question_id
+    
+    unless foreign_key_exists?(:test_passings, :questions, column: :current_question_id)
+      add_foreign_key :test_passings, :questions, column: :current_question_id
+    end
   end
 end
